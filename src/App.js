f3cf36a534c483345991b53ed6a13339668aba9c
rameezqazi23 <rameez.qazi23@gmail.com>
2023-07-21 17:28:02 +0500

    getting statrted with react base app, creating travel landing page with tailwind css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HeroSection from "./Components/HeroSection";
+
+
+function App() {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className='w-full h-screen'>
+            <img className='top-0 left-0 w-full h-screen object-cover' src="/images/image-1.jpg" alt="/" />
+            <div className='bg-black/30 absolute top-0 left-0 w-full h-screen' />
+            <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
+                <div className='md:left-[10%] max-w-[1100px] m-auto absolute p-4'>
+                    <p>All Inclusive</p>
+                    <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>Private Beaches & Gateways</h1>
+                    <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl'>Lorem ipsum dolor sit amet  consectetur, adipisicing elit. Odio molestiae ipsa sit tenetur cupiditate fugit, dolore amet quae corporis quidem!</p>
+                    <button className='bg-white text-black font-bold'>Reserve Now</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default HeroSection
